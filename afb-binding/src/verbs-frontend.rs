56e541459013f24d2158c4f9138319ae377bda0c
Fulup Ar Foll <fulup@iot.bzh>
2024-01-26 19:03:25 +0100

    WIP test

--- afb-binding/src/verbs-frontend.rs
+++ afb-binding/src/verbs-frontend.rs
@@ -17,38 +17,38 @@ use typesv4::prelude::*;
 
 // init ocpp backend at API initialization time
 pub fn ocpp_bootstrap(api: &AfbApi, station: &str) -> Result<(), AfbError> {
-            AfbSubCall::call_sync(
-            api,
-            "OCPP-C",
-            "BootNotification",
-            v106::BootNotification::Request(v106::BootNotificationRequest {
-                charge_point_vendor: station.to_string(),
-                charge_point_model: "Tux-Evse OCPP-1.6".to_string(),
-                firmware_version: Some("v1234".to_string()),
-                charge_box_serial_number: None,
-                charge_point_serial_number: None,
-                iccid: None,
-                imsi: None,
-                meter_serial_number: None,
-                meter_type: None,
-            }),
-        )?;
+    AfbSubCall::call_sync(
+        api,
+        "OCPP-C",
+        "BootNotification",
+        v106::BootNotification::Request(v106::BootNotificationRequest {
+            charge_point_vendor: station.to_string(),
+            charge_point_model: "Tux-Evse OCPP-1.6".to_string(),
+            firmware_version: Some("v1234".to_string()),
+            charge_box_serial_number: None,
+            charge_point_serial_number: None,
+            iccid: None,
+            imsi: None,
+            meter_serial_number: None,
+            meter_type: None,
+        }),
+    )?;
 
-        AfbSubCall::call_sync(
-            api,
-            "OCPP-C",
-            "StatusNotification",
-            v106::StatusNotification::Request(v106::StatusNotificationRequest {
-                connector_id: 1,
-                status: v106::ChargePointStatus::Preparing,
-                error_code: v106::ChargePointErrorCode::NoError,
-                info: Some("Tux-Evse booting report".to_string()),
-                timestamp: Some(get_utc()),
-                vendor_id: None,
-                vendor_error_code: None,
-            }),
-        )?;
-        Ok(())
+    AfbSubCall::call_sync(
+        api,
+        "OCPP-C",
+        "StatusNotification",
+        v106::StatusNotification::Request(v106::StatusNotificationRequest {
+            connector_id: 1,
+            status: v106::ChargePointStatus::Preparing,
+            error_code: v106::ChargePointErrorCode::NoError,
+            info: Some("Tux-Evse booting report".to_string()),
+            timestamp: Some(get_utc()),
+            vendor_id: None,
+            vendor_error_code: None,
+        }),
+    )?;
+    Ok(())
 }
 
 struct TimerCtx {
@@ -70,7 +70,7 @@ AfbCallRegister!(MeterValuesRsp, meter_values_response);
 fn meter_values_response(_api: &AfbApi, args: &AfbData) -> Result<(), AfbError> {
     let data = args.get::<&v106::MeterValues>(0)?;
     match data {
-        v106::MeterValues::Response(_response) => {},
+        v106::MeterValues::Response(_response) => {}
         _ => return afb_error!("ocpp-metervalue-rsp", "invalid response type"),
     };
     Ok(())
@@ -83,9 +83,9 @@ struct EngyEvtCtx {
 // ref: https://www.ampcontrol.io/ocpp-guide/how-to-send-ocpp-meter-values-with-metervalues-req
 AfbEventRegister!(EngyEvtCtrl, engy_event_cb, EngyEvtCtx);
 fn engy_event_cb(evt: &AfbEventMsg, args: &AfbData, ctx: &mut EngyEvtCtx) -> Result<(), AfbError> {
-    let tid= ctx.mgr.get_tid()?;
+    let tid = ctx.mgr.get_tid()?;
     if tid == 0 {
-        return afb_error! ("ocpp-energy-state", "not active transaction running")
+        return afb_error!("ocpp-energy-state", "not active transaction running");
     }
 
     let state = args.get::<&EnergyState>(0)?;
@@ -93,57 +93,52 @@ fn engy_event_cb(evt: &AfbEventMsg, args: &AfbData, ctx: &mut EngyEvtCtx) -> Res
 
     let tension_value = v106::SampledValue {
         value: (state.tension / 100).to_string(),
-        location: None,  // string
-        context: None,   // string
-        phase: None,     // default L1
-        format: None, // default raw
+        location: None, // string
+        context: None,  // string
+        phase: None,    // default L1
+        format: None,   // default raw
         unit: Some(v106::UnitOfMeasure::V),
         measurand: Some(v106::Measurand::Voltage),
     };
 
     let current_value = v106::SampledValue {
         value: (state.current / 100).to_string(),
-        location: None,  // string
-        context: None,   // string
-        phase: None,     // default L1
-        format: None, // default Raw
+        location: None, // string
+        context: None,  // string
+        phase: None,    // default L1
+        format: None,   // default Raw
         unit: Some(v106::UnitOfMeasure::A),
         measurand: Some(v106::Measurand::CurrentImport),
     };
 
     let power_value = v106::SampledValue {
         value: (state.power / 100).to_string(),
-        location: None,  // string
-        context: None,   // string
-        phase: None,     // default L1
-        format: None,// default Raw
+        location: None, // string
+        context: None,  // string
+        phase: None,    // default L1
+        format: None,   // default Raw
         unit: Some(v106::UnitOfMeasure::Kw),
         measurand: Some(v106::Measurand::PowerActiveImport),
     };
 
     let session_value = v106::SampledValue {
         value: (state.session / 100).to_string(),
-        location: None,  // string
-        context: None,   // string
-        phase: None,     // default L1
-        format: None,    // default Raw
+        location: None, // string
+        context: None,  // string
+        phase: None,    // default L1
+        format: None,   // default Raw
         unit: Some(v106::UnitOfMeasure::KWh),
-        measurand: Some(v106::Measurand::EnergyActiveExportRegister),
+        measurand: Some(v106::Measurand::EnergyActiveImportRegister),
     };
 
     let query = v106::MeterValuesRequest {
-            connector_id: ctx.mgr.get_cid(),
-            transaction_id: Some(tid),
-            meter_value: vec![v106::MeterValue {
-                timestamp: get_utc(),
-                sampled_value: vec![
-                    tension_value,
-                    power_value,
-                    current_value,
-                    session_value,
-                ],
-            }],
-        };
+        connector_id: ctx.mgr.get_cid(),
+        transaction_id: Some(tid),
+        meter_value: vec![v106::MeterValue {
+            timestamp: get_utc(),
+            sampled_value: vec![tension_value, power_value, current_value, session_value],
+        }],
+    };
 
     AfbSubCall::call_async(
         evt.get_apiv4(),
@@ -156,13 +151,56 @@ fn engy_event_cb(evt: &AfbEventMsg, args: &AfbData, ctx: &mut EngyEvtCtx) -> Res
     Ok(())
 }
 
+struct HeartbeatRspCtx {
+    nonce: u32,
+}
+AfbVerbRegister!(HeartbeatRsp, heartbeat_response, HeartbeatRspCtx);
+fn heartbeat_response(
+    rqt: &AfbRequest,
+    args: &AfbData,
+    ctx: &mut HeartbeatRspCtx,
+) -> Result<(), AfbError> {
+    let data = args.get::<&v106::Heartbeat>(0)?;
+    match data {
+        v106::Heartbeat::Response(response) => response,
+        _ => return afb_error!("ocpp-heartbeat-response", "invalid response type"),
+    };
+
+    afb_log_msg!(Debug, rqt, "Heartbeat request nonce:{}", ctx.nonce);
+    rqt.reply(ctx.nonce, 0);
+    Ok(())
+}
+
+// Authentication check id_tag on backend
+AfbVerbRegister!(HeartbeatRqt, heartbeat_request);
+fn heartbeat_request(
+    rqt: &AfbRequest,
+    args: &AfbData,
+) -> Result<(), AfbError> {
+    let nonce = args.get::<u32>(0)?;
+    afb_log_msg!(Debug, rqt, "Heartbeat request nonce:{}", nonce);
+
+    let query = v106::HeartbeatRequest {};
+    AfbSubCall::call_async(
+        rqt,
+        "OCPP-C",
+        "Heartbeat",
+        v106::Heartbeat::Request(query),
+        Box::new(HeartbeatRspCtx { nonce }),
+    )?;
+    Ok(())
+}
 
 // Authorize async start response callback
 struct AuthorizeRspCtx {
     mgr: &'static ManagerHandle,
 }
 AfbVerbRegister!(AuthorizeRsp, authorize_response, AuthorizeRspCtx);
-fn authorize_response(rqt: &AfbRequest, args: &AfbData, ctx: &mut AuthorizeRspCtx) -> Result<(), AfbError> {
+fn authorize_response(
+    rqt: &AfbRequest,
+    args: &AfbData,
+    ctx: &mut AuthorizeRspCtx,
+) -> Result<(), AfbError> {
     let data = args.get::<&v106::Authorize>(0)?;
     let response = match data {
         v106::Authorize::Response(response) => response,
@@ -174,10 +212,13 @@ fn authorize_response(rqt: &AfbRequest, args: &AfbData, ctx: &mut AuthorizeRspCt
             ctx.mgr.authorized(true)?;
         }
         _ => {
-            return afb_error!("ocpp-authorize-start", "fail auth:{:?}", response.id_tag_info.status)
+            return afb_error!(
+                "ocpp-authorize-start",
+                "fail auth:{:?}",
+                response.id_tag_info.status
+            )
         }
     };
-
     rqt.reply(AFB_NO_DATA, 0);
     Ok(())
 }
@@ -195,9 +236,7 @@ fn authorize_request(
     let id_tag = args.get::<String>(0)?;
 
     afb_log_msg!(Debug, rqt, "Authorize request tag:{}", id_tag);
-    let query = v106::AuthorizeRequest {
-        id_tag,
-    };
+    let query = v106::AuthorizeRequest { id_tag };
 
     AfbSubCall::call_async(
         rqt,
@@ -206,29 +245,33 @@ fn authorize_request(
         v106::Authorize::Request(query),
         Box::new(AuthorizeRspCtx { mgr: ctx.mgr }),
     )?;
-
     Ok(())
 }
-
 
 // Transaction async start response callback
 struct TransacStartRspCtx {
     mgr: &'static ManagerHandle,
 }
 AfbVerbRegister!(TransacStartRsp, transac_start_rsp, TransacStartRspCtx);
-fn transac_start_rsp(rqt: &AfbRequest, args: &AfbData, ctx: &mut TransacStartRspCtx) -> Result<(), AfbError> {
+fn transac_start_rsp(
+    rqt: &AfbRequest,
+    args: &AfbData,
+    ctx: &mut TransacStartRspCtx,
+) -> Result<(), AfbError> {
     let data = args.get::<&v106::StartTransaction>(0)?;
     let response = match data {
         v106::StartTransaction::Response(response) => response,
         _ => return afb_error!("ocpp-transaction-start", "invalid response type"),
     };
 
-    let tid= match response.id_tag_info.status {
-        v106::AuthorizationStatus::Accepted => {
-            response.transaction_id
-        }
+    let tid = match response.id_tag_info.status {
+        v106::AuthorizationStatus::Accepted => response.transaction_id,
         _ => {
-            return afb_error!("ocpp-transaction-start", "fail auth:{:?}", response.id_tag_info.status)
+            return afb_error!(
+                "ocpp-transaction-start",
+                "fail auth:{:?}",
+                response.id_tag_info.status
+            )
         }
     };
     ctx.mgr.login(tid)?;
@@ -241,7 +284,11 @@ struct TransacStopRspCtx {
     mgr: &'static ManagerHandle,
 }
 AfbVerbRegister!(TransacStopRsp, transac_stop_rsp, TransacStopRspCtx);
-fn transac_stop_rsp(rqt: &AfbRequest, args: &AfbData, ctx: &mut TransacStopRspCtx) -> Result<(), AfbError> {
+fn transac_stop_rsp(
+    rqt: &AfbRequest,
+    args: &AfbData,
+    ctx: &mut TransacStopRspCtx,
+) -> Result<(), AfbError> {
     let data = args.get::<&v106::StopTransaction>(0)?;
     match data {
         v106::StopTransaction::Response(response) => response,
@@ -271,7 +318,7 @@ fn transaction_request(
             let query = v106::StartTransactionRequest {
                 connector_id: ctx.mgr.get_cid(),
                 id_tag: tag.clone(),
-                meter_start:0,
+                meter_start: 0,
                 reservation_id: None,
                 timestamp: get_utc(),
             };
@@ -287,7 +334,7 @@ fn transaction_request(
         OcppTransaction::Stop(meter) => {
             afb_log_msg!(Debug, rqt, "Stop Transaction request");
             ctx.mgr.check_active_session(true)?;
-            let query= v106::StopTransactionRequest {
+            let query = v106::StopTransactionRequest {
                 id_tag: None,
                 meter_stop: *meter,
                 timestamp: get_utc(),
@@ -321,29 +368,32 @@ pub(crate) fn register_frontend(
 
     let engy_handler = AfbEvtHandler::new("energy-evt")
         .set_pattern(to_static_str(format!("{}/*", config.engy_api)))
-        .set_callback(Box::new(EngyEvtCtx {
-            mgr: config.mgr
-        }))
+        .set_callback(Box::new(EngyEvtCtx { mgr: config.mgr }))
         .finalize()?;
 
-    let authorize_verb = AfbVerb::new("Authorize")
-        .set_callback(Box::new(AuthorizeRqtCtx {mgr: config.mgr}))
-        .set_info("Request authorize config from backend")
-        .set_sample("{'idTag':'tux-evse-1'}")?
-        .set_sample("{'idTag':'tux-evse-2'}")?
-        .set_usage("{'idTag':'xxx'}")
+    let heartbeat_verb = AfbVerb::new("heartbeat")
+        .set_callback(Box::new(HeartbeatRqt{}))
+        .set_info("Request tagid authorization from backend")
         .finalize()?;
 
-    let transaction_verb = AfbVerb::new("Transaction")
-        .set_callback(Box::new(TransacRqtCtx {mgr: config.mgr}))
+    let authorize_verb = AfbVerb::new("authorize")
+        .set_callback(Box::new(AuthorizeRqtCtx { mgr: config.mgr }))
+        .set_info("Request tagid authorization from backend")
+        .set_sample("tux-evse-1")?
+        .set_sample("tux-evse-2")?
+        .set_usage("idTag")
+        .finalize()?;
+
+    let transaction_verb = AfbVerb::new("transaction")
+        .set_callback(Box::new(TransacRqtCtx { mgr: config.mgr }))
         .set_info("send start/stop transaction to backend")
-        .set_sample("{'id_tag': 'Tux-EvSe-Tag', 'connector_id':0, 'charging_profile': 'None'}")?
-        .set_usage("{'connector_id': 'string', 'connector_id':if(optional), 'charging_profile', ChargingProfile(optional)}")
+        .set_usage("idTag")
         .finalize()?;
 
     // register veb within API
     api.add_verb(authorize_verb);
     api.add_verb(transaction_verb);
+    api.add_verb(heartbeat_verb);
     api.add_evt_handler(engy_handler);
 
     Ok(())
--- afb-test/src/ocpp-16-test.rs
+++ afb-test/src/ocpp-16-test.rs
@@ -6,17 +6,10 @@
  * License: $RP_BEGIN_LICENSE$ SPDX:MIT https://opensource.org/licenses/MIT $RP_END_LICENSE$
  */
 
-#![doc(
-    html_logo_url = "https://iot.bzh/images/defaults/company/512-479-max-transp.png",
-    html_favicon_url = "https://iot.bzh/images/defaults/favicon.ico"
-)]
-
-#[cfg(not(afbv4))]
-extern crate afbv4;
 
 use afbv4::prelude::*;
 use ocpp::prelude::*;
-use chrono::Utc;
+use typesv4::prelude::*;
 
 // This rootv4 demonstrate how to test an external rootv4 that you load within the same afb-binder process and security context
 // It leverages test (Test Anything Protocol) that is compatible with redpesk testing report.
@@ -31,76 +24,39 @@ impl AfbApiControls for TapUserData {
     fn start(&mut self, api: &AfbApi) -> Result<(), AfbError> {
         afb_log_msg!(Notice, api, "starting OCPP testing");
 
-        let boot_notification = AfbTapTest::new("boot-notification", "ocpp-16", "BootNotification")
-            .set_info("send boot notification to backend")
-            .add_arg(v106::BootNotification::Request(v106::BootNotificationRequest {
-                charge_point_vendor: "Tux-EvSe".to_string(),
-                charge_point_model: "ocpp-16 Rust-ocpp test".to_string(),
-                firmware_version: Some("v1234".to_string()),
-                charge_box_serial_number: None,
-                charge_point_serial_number: None,
-                iccid: None,
-                imsi: None,
-                meter_serial_number: None,
-                meter_type: None,
-            }))?
-            .add_expect(&JsonStr("{'status':'Accepted'}"))?
+        // check tad_id on server
+        let tagid_check = AfbTapTest::new("authentication-request", "ocpp", "authorize")
+            .set_info("send authentication request")
+            .add_arg("tux-evse-001")?
             .finalize()?;
 
-        let status_notification =
-            AfbTapTest::new("status-notification", "ocpp-16", "StatusNotification")
-                .set_info("send status notification to backend")
-                .add_arg(v106::StatusNotification::Request(
-                    v106::StatusNotificationRequest {
-                        connector_id: 1,
-                        status: v106::ChargePointStatus::Preparing,
-                        error_code: v106::ChargePointErrorCode::NoError,
-                        info: Some("Dummy Rust ocpp status report".to_string()),
-                        timestamp: Some(Utc::now()),
-                        vendor_id: None,
-                        vendor_error_code: None,
-                    },
-                ))?
-                // does not returned any status
-                .finalize()?;
+        // start transaction with tagid
+        let start_transaction = AfbTapTest::new("transaction-start", "ocpp", "transaction")
+            .set_info("send start transaction")
+            .add_arg(OcppTransaction::Start("tux-evse-001".to_string()))?
+            .finalize()?;
 
-        let heartbeat = AfbTapTest::new("heartbeat", "ocpp-16", "HeartBeat")
+        // stop transaction send consumes power
+        let stop_transaction = AfbTapTest::new("transaction-stop", "ocpp", "transaction")
+            .set_info("send stop transaction")
+            .add_arg(OcppTransaction::Stop(1234))?
+            .finalize()?;
+
+        let heartbeat = AfbTapTest::new("heartbeat", "ocpp", "heartBeat")
             .set_info("send heartbeat to backend")
-            .add_arg(v106::Heartbeat::Request(v106::HeartbeatRequest{
-            }))?
-            .finalize()?;
-
-        let start_transaction = AfbTapTest::new("start-transaction", "ocpp-16", "StartTransaction")
-            .set_info("send start transaction to backend")
-            .add_arg(v106::StartTransaction::Request(v106::RemoteStartTransactionRequest{
-                id_tag: "Tux-EvSe-Tag".to_string(),
-                connector_id:1,
-                charging_profile: None,
-            }))?
-            .add_expect(&JsonStr("{'status':'Accepted'}"))?
-            .finalize()?;
-
-        let stop_transaction = AfbTapTest::new("stop-transaction", "ocpp-16", "StopTransaction")
-            .set_info("send start transaction to backend")
-            .add_arg(v106::StopTransaction::Request(v106::RemoteStopTransactionRequest{
-                transaction_id: 1234,
-            }))?
-            .add_expect(&JsonStr("{'status':'Accepted'}"))?
             .finalize()?;
 
         AfbTapSuite::new(api, "Tap Demo Test")
-            .set_info("Check Example demo API works")
+            .set_info("OCPP frontend -> occp server test")
             .set_timeout(0)
-            .add_test(boot_notification)
-            .add_test(status_notification)
             .add_test(heartbeat)
+            .add_test(tagid_check)
             .add_test(start_transaction)
             .add_test(stop_transaction)
             .set_autorun(self.autostart)
             .set_autoexit(self.autoexit)
             .set_output(self.output)
             .finalize()?;
-
         Ok(())
     }
 
