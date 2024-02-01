eabee3ffa3ae3406aa85320c4cb17106e956c0f7
Fulup Ar Foll <fulup@iot.bzh>
2024-02-01 14:28:45 +0100

    fix afb-test start script

---
+++ afb-test/etc/start-test-ocpp-16.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/usr/local/lib64
+pkill afb-ocpp
+cynagora-admin set '' 'HELLO' '' '*' yes
+clear
+
+# build test config dirname
+DIRNAME=`dirname $0`
+cd $DIRNAME/..
+ROOTDIR=`pwd`
+CONFDIR=`pwd`/etc
+mkdir -p /tmp/api
+
+DEVTOOL_PORT=1235
+echo Ocpp debug mode config=$CONFDIR/*.json port=$DEVTOOL_PORT
+
+afb-binder --name=afb-ocpp --port=$DEVTOOL_PORT -v \
+  --config=$ROOTDIR/../afb-binding/etc/binder-ocpp.json \
+  --config=$CONFDIR/binding-bia-power.json \
+  --config=$CONFDIR/binding-test-ocpp-16.json \
+  $*
